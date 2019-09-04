09f6c1833e258bedeee7257e81d40169ec5a80eb
ahmed waqas nasir <ahmed.nasir@venturedive.com>
2019-09-04 10:37:36 +0500

    semantic versioning and package installation

--- app.js
+++ app.js
@@ -1,35 +1,4 @@
-const math = require("./math");
-const EventEmitter = require("events");
+const _ = require("lodash");
 
-// node setup
-console.log("Hello world from node");
-
-// module section
-console.log(math.sum(3, 7));
-console.log(math.multiply(3, 7));
-
-// event emitter
-const eventEmitter = new EventEmitter();
-eventEmitter.on("sendEmail", () => console.log("Email sending process"));
-eventEmitter.emit("sendEmail");
-
-// event emitter with class
-class Person extends EventEmitter {
-  constructor(name) {
-    super()
-    this.name = name;
-  }
-  getName () {
-    console.log(this.name);
-  }
-}
-
-const ali = new Person('ali');
-const usman = new Person('usman');
-ali.on('name', ()=>{
-  console.log('my name is '+ ali.name)
-})
-ali.emit('name');
-ali.emit('name');
-usman.emit('name')
-
+const result = _.partition([1, 2, 3, 4], n => n % 2);
+console.log(result)
